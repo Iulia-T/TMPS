7e9e1dc5fe8bd73d92f6827ac2cb5aa82a69241f
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2022-10-13 12:13:28 +0300

    Update DeliveryApp.java

--- Laboratory1/SingleResponsabilityPrinciple/DeliveryApp.java
+++ Laboratory1/SingleResponsabilityPrinciple/DeliveryApp.java
@@ -7,18 +7,8 @@ class DeliveryApp {
   
     public void delivery() 
     { 
-        // Here, we would want to interface with another 
-        // system which actually assigns the task of 
-        // delivery to different persons 
-        // based on location, etc. 
         System.out.println("Delivering the order"); 
-        System.out.println( 
-            "Order with order id as "
-            + this.order.getOrderId() 
-            + " being delivered to "
-            + this.order.getCustomer().getName()); 
-        System.out.println( 
-            "Order is to be delivered to: "
-            + this.order.getCustomer().getAddress()); 
+        System.out.println("Order with order id as " + this.order.getOrderId() + " being delivered to " + this.order.getCustomer().getName()); 
+        System.out.println("Order is to be delivered to: " + this.order.getCustomer().getAddress()); 
     } 
 }
