2c6ad08546ee0a9b7352d6b0202f5175762a442d
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2022-10-13 12:10:18 +0300

    Update Application.java

--- Laboratory1/OpenClosedPrinciple/Application.java
+++ Laboratory1/OpenClosedPrinciple/Application.java
@@ -5,10 +5,8 @@ class Application {
     public double
         get_total_volume(Geo_objects[] geo_objects)
     {
-        // Initially initializing sum to zero 
         double vol_sum = 0;
  
-        // Iterating using for each loop 
         for (Geo_objects geo_obj : geo_objects) {
             vol_sum += geo_obj.get_volume();
         }
