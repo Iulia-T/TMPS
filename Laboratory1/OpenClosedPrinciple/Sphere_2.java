834965264620bca353ae58ae41503603541bb7c4
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2022-10-13 12:11:28 +0300

    Update Sphere_2.java

--- Laboratory1/OpenClosedPrinciple/Sphere_2.java
+++ Laboratory1/OpenClosedPrinciple/Sphere_2.java
@@ -1,13 +1,9 @@
 package Laboratory1.OpenClosedPrinciple;
 
 class Sphere_2 extends Geo_objects {
-     
-    // To store radius of a sphere
+
     public double radius;
- 
-    // Overrided function to calculate
-    //  the volume of a sphere
-     
+
     //@Override
     public double get_volume()
     {
