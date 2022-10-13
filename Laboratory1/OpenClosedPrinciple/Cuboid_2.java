3d67b5d7f19cb905a67716f6dd5a96e6452ad5ee
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2022-10-13 12:10:45 +0300

    Update Cuboid_2.java

--- Laboratory1/OpenClosedPrinciple/Cuboid_2.java
+++ Laboratory1/OpenClosedPrinciple/Cuboid_2.java
@@ -1,15 +1,11 @@
 package Laboratory1.OpenClosedPrinciple;
 
 class Cuboid_2 extends Geo_objects {
-     
-    // used to store length, breadth and height of a cuboid
  
     public double length;
     public double breadth;
     public double height;
- 
-    // overrided function to calculate 
-    // the volume of a cuboid
+
     // @Override
     public double get_volume()
     {
