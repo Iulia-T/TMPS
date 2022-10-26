ea62dc678bff188491764db2a28ab5565e9078ff
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2022-10-26 23:16:18 +0300

    Update Application.java

--- Laboratory1/OpenClosedPrinciple/Application.java
+++ Laboratory1/OpenClosedPrinciple/Application.java
@@ -1,16 +1,1 @@
-package Laboratory1.OpenClosedPrinciple;
 
-class Application {
- 
-    public double
-        get_total_volume(Geo_objects[] geo_objects)
-    {
-        double vol_sum = 0;
- 
-        for (Geo_objects geo_obj : geo_objects) {
-            vol_sum += geo_obj.get_volume();
-        }
-  
-        return vol_sum;
-    }
-}
